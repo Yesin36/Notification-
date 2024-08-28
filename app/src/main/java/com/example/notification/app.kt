02cb0e02c9ca62dc2dc8d03917686259c0c1cae9
Nishat <shourovhossainnishat@gmail>
2024-08-28 19:35:57 +0600

    Create channel

---
+++ app/src/main/java/com/example/notification/app.kt
@@ -0,0 +1,31 @@
+package com.example.notification
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+
+class app: Application() {
+    final  private val CHANNEL_ID1 = "CHANNEL_ID1"
+    final  private val CHANNEL_ID2 = "CHANNEL_ID2"
+
+    override fun onCreate() {
+        super.onCreate()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel1 = NotificationChannel(CHANNEL_ID1,"channel1", NotificationManager.IMPORTANCE_HIGH)
+            val channel2 = NotificationChannel(CHANNEL_ID2,"channel2", NotificationManager.IMPORTANCE_DEFAULT)
+
+            channel1.description =" This is high importance channel"
+            channel2.description = "This is default importance channel"
+
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel1)
+            manager.createNotificationChannel(channel2)
+
+
+
+        }
+    }
+
+
+}
